6791b4d1b39735a3abc8b475753dc11801d2c03b
Ashsalcedo <106289479+Ashsalcedo@users.noreply.github.com>
2025-05-24 20:03:01 -0600

    Update cargar_analistas.php

--- administrador/cargar_analistas.php
+++ administrador/cargar_analistas.php
@@ -2,13 +2,13 @@
 include 'conexion.php';
 
 // Consultar IDs de los analistas
-$sql = "SELECT id_analista FROM analistas";
+$sql = "SELECT * FROM analistas";
 $result = $conn->query($sql);
 
 $options = "";
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $options .= "<option value='{$row['id_analista']}'>{$row['id_analista']}</option>";
+        $options .= "<option value='{$row['id_analista']}'>{$row['id_analista']}/{$row['nombre_analista']} {$row['ap_analista']} </option>";
     }
 } else {
     $options = "<option value=''>No hay analistas disponibles</option>";
